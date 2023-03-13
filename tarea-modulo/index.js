1c12c72dd1df3348c00e2f26cd2cc2dded9a80f9
julianny19 <camachojulianny26@gmail.com>
2023-03-13 16:36:22 +0100

    modulos

---
+++ tarea-modulo/index.js
@@ -0,0 +1,6 @@
+import * as control from './modulos/conroller.js'
+import chalk from 'chalk';
+const sum= control.suma(1,2);
+console.log(sum);
+const multpli= control.multiplica(4,5);
+console.log(chalk.green(multpli));
