a1dcca02a099e2c6cad2de7d944cb3e9d7556a76
julianny19 <camachojulianny26@gmail.com>
2023-03-20 17:33:26 +0100

    html

---
+++ eventos/index.js
@@ -0,0 +1,8 @@
+const boton= document.getElementById("btn")
+boton.addEventListener("click", ()=>{
+    alert("click en el boton")
+})
+
+$("#bjquery").click(()=>{
+    console.log("Hola estoy utilizando Jquery")
+})
